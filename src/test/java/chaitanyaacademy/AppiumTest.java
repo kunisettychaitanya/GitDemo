daad640717f1eb8cd7b20b32f0a37d07a47bac7a
chaitanya <kunisettychaitanya@gmail.com>
2020-04-16 14:00:03 +0530

    first commit

---
+++ src/test/java/chaitanyaacademy/AppiumTest.java
@@ -0,0 +1,17 @@
+package chaitanyaacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void relativeAppAndroid() {
+		System.out.println("relativeAppAndroid");
+	}
+
+	@Test
+	public void iosApps() {
+		System.out.println("iosApps");
+	}
+
+
+}
---
+++ src/test/java/chaitanyaacademy/SeleniumTest.java
@@ -0,0 +1,17 @@
+package chaitanyaacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void browserAutomation() {
+		System.out.println("browserAutomation");
+	}
+
+	@Test
+	public void elementsUI() {
+		System.out.println("elementsUI");
+	}
+
+}
